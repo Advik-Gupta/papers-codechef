bd1b4b96becb5bd427d193940dcba8a3e7db36fa
abhitrueprogrammer <abhiaruvs3@gmail.com>
2025-09-08 23:50:18 +0530

    color: purple

--- src/components/CookoffBanner.tsx
+++ src/components/CookoffBanner.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 export default function CookoffBanner() {
   return (
     <>
-      <div className="z-50 flex h-fit w-full items-center justify-center bg-[#471600] px-6 py-3 text-center text-white sm:h-14 sm:py-0 md:sticky md:top-0 md:justify-between md:text-left">
+      <div className="z-50 flex h-fit w-full items-center justify-center bg-[#626bd2] px-6 py-3 text-center text-white sm:h-14 sm:py-0 md:sticky md:top-0 md:justify-between md:text-left">
         <div className="flex items-center gap-x-2">
           <Image
             src="/cookoff.png"
@@ -30,12 +30,12 @@ export default function CookoffBanner() {
             target="_blank"
             
           >
-            Register for CookOff 10.0
+            Register for CookOff 10.0u
           </Link>
         </div>
 
         <div className="hidden md:block">
-          <Button className="bg-[#e5b0a0] hover:bg-[#e68e73] focus:ring-2 focus:ring-offset-2 focus:ring-[#e5b0a0]">
+          <Button className="bg-[#5c34e7] hover:bg-[#a6b0ff] focus:ring-2 focus:ring-offset-2 focus:ring-[#b2b8ff]">
             <Link
               className="flex items-center gap-x-2"
               href="https://gravitas.vit.ac.in/events/bdfcebea-c141-4a61-ac73-61dec96c08f4"

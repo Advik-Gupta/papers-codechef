ffe7549e84222ac95117658359ee65b632142ae8
abhitrueprogrammer <abhiaruvs3@gmail.com>
2025-10-03 14:41:05 +0530

    standalone

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ await import("./src/env.js");
 /** @type {import("next").NextConfig} */
 const config = {
   swcMinify: false,
+  output: "standalone", 
   images: {
     domains: ["storage.googleapis.com"],
   },
